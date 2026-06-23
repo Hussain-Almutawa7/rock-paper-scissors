aa6fcf1b229a31adeb8874aeb89ed81f443ad4bf
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-06-23 10:53:48 +0300

    Enhanced game logic and implement reset button

--- app.js
+++ app.js
@@ -2,23 +2,49 @@ const choices = document.querySelector("#button-action");
 let userChoice;
 
 const computerChoice = ["rock", "paper", "scissor"];
-const computerSelect = Math.floor(Math.random() * computerChoice.length)
-let computerPick = computerChoice[computerSelect];
+
+const userScore = document.querySelector("#user-score")
+const computerScore = document.querySelector("#computer-score")
+const tieScore = document.querySelector("#tie-score")
+const resetScore = document.querySelector("#reset-score");
+
+let userPoints = 0;
+let computerPoints = 0;
+let tiePoints = 0;
 
 choices.addEventListener("click", e => {
     userChoice = e.target.id;
+
+    const computerSelect = Math.floor(Math.random() * computerChoice.length)
+    let computerPick = computerChoice[computerSelect];
+
+    gameLogic(userChoice, computerPick)
 })
 
 function gameLogic(user, computer) {
     if(user === computer) {
-        console.log("draw")
+        tiePoints++;
     } else if (
         (user === "rock" && computer === "scissors") || 
         (user === "paper" && computer === "rock") || 
         (user === "scissors" && computer === "paper")
     ) {
-        console.log("You win")
+        userPoints++
     } else {
-        console.log("you lose")
+        computerPoints++
     }
+
+    userScore.textContent = userPoints;
+    computerScore.textContent = computerPoints;
+    tieScore.textContent = tiePoints;
 }
+
+resetScore.addEventListener("click", () => {
+    userPoints = 0;
+    computerPoints = 0;
+    tiePoints = 0;
+
+    userScore.textContent = userPoints;
+    computerScore.textContent = computerPoints;
+    tieScore.textContent = tiePoints;
+})
