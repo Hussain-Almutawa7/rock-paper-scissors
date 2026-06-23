36282e4ab8c8ee0a77491578ca9d766c3d32578b
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-06-23 12:05:28 +0300

    Added display choices

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const choices = document.querySelector("#button-action");
-let userChoice;
-
-const computerChoice = ["rock", "paper", "scissor"];
+const diplayChoice = document.querySelector("#display-choice");
 
 const userScore = document.querySelector("#user-score")
 const computerScore = document.querySelector("#computer-score")
 const tieScore = document.querySelector("#tie-score")
 const resetScore = document.querySelector("#reset-score");
+
+const winner = document.querySelector("#result");
+const userSelect = document.querySelector("#display-user")
+const computerSelect = document.querySelector("#display-computer")
+
+const computerChoice = ["rock", "paper", "scissors"];
+let userChoice;
 
 let userPoints = 0;
 let computerPoints = 0;
@@ -24,20 +29,46 @@ choices.addEventListener("click", e => {
 function gameLogic(user, computer) {
     if(user === computer) {
         tiePoints++;
+        winner.textContent = "Tie"
     } else if (
         (user === "rock" && computer === "scissors") || 
         (user === "paper" && computer === "rock") || 
         (user === "scissors" && computer === "paper")
     ) {
         userPoints++
+        winner.textContent = "You Win"
     } else {
         computerPoints++
+        winner.textContent = "You Lose"
     }
+
+    displayUserChoice(user)
+    displayComputerChoice(computer)
 
     userScore.textContent = userPoints;
     computerScore.textContent = computerPoints;
     tieScore.textContent = tiePoints;
 }
+
+//Display Choices
+function displayUserChoice(user) {
+    if(user === "rock")
+        userSelect.textContent = "You ✊"
+    else if (user === "paper")
+        userSelect.textContent = "You ✋"
+    else
+        userSelect.textContent = "You ✌️"
+}
+
+function displayComputerChoice(computer) {
+    if(computer === "rock")
+        computerSelect.textContent = "✊ Computer"
+    else if (computer === "paper")
+        computerSelect.textContent = "✋ Computer"
+    else
+        computerSelect.textContent = "✌️ Computer"
+}
+
 
 resetScore.addEventListener("click", () => {
     userPoints = 0;
@@ -47,4 +78,9 @@ resetScore.addEventListener("click", () => {
     userScore.textContent = userPoints;
     computerScore.textContent = computerPoints;
     tieScore.textContent = tiePoints;
+
+    userSelect.textContent = ""
+    computerSelect.textContent = ""
+    
+    winner.textContent = "";
 })
